3c6be1b3f32cbd343d67eca62544e5efad4fd7ba
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-02 19:25:07 +0100

    Splash Script - Capitalisation of const

--- menus/splash/splash.gd
+++ menus/splash/splash.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const main_menu_path = "res://"
+const MAIN_MENU_PATH = "res://"
 
 func _ready():
 	open_main_menu()
@@ -11,4 +11,4 @@ func _input(event):
 		open_main_menu()
 
 func open_main_menu():
-	get_tree().change_scene(main_menu_path)
+	get_tree().change_scene(MAIN_MENU_PATH)
