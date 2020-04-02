04defc69418476f00bf295f6c4f8d7d4b597bb2b
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-02 19:12:40 +0100

    Splash Script - Open menu function
    - Added function to change to main menu
    - Changed load scene in input function

--- menus/splash/splash.gd
+++ menus/splash/splash.gd
@@ -1,11 +1,12 @@
 extends Node
 
 func _ready():
-	#Sets the game settings from the config file
-	settings.check_for_file()
-
+	open_main_menu()
 
 func _input(event):
 	if event.is_action("ui_left_select"):
 		$animation_player.stop()
-		loading_screen.goto_scene("res://")
+		open_main_menu()
+
+func open_main_menu():
+	loading_screen.goto_scene("res://")
