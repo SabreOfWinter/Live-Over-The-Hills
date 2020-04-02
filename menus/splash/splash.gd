4c61a5bbcb2275055a210122504d69e9481eee31
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-02 19:10:10 +0100

    Splash Script - ready function added

--- menus/splash/splash.gd
+++ menus/splash/splash.gd
@@ -1,5 +1,10 @@
 extends Node
 
+func _ready():
+	#Sets the game settings from the config file
+	settings.check_for_file()
+
+
 func _input(event):
 	if event.is_action("ui_left_select"):
 		$animation_player.stop()
