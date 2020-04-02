bac92cdaac1bf4c2279e6362a738d859a19746ea
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-02 19:26:52 +0100

    Settings Script - File variables added
    - Error states
    - File path
    - File object
    - Default settings dictionary

--- settings.gd
+++ settings.gd
@@ -1,2 +1,24 @@
 extends Node
 
+enum {LOAD_SUCCESS, LOAD_ERROR_COULDNT_OPEN}
+const SAVE_PATH = "user://settings.cfg"
+var settings_file = ConfigFile.new()
+var settings_dict = {
+	"Sound":{
+		"Master" : 100,
+		"Ambient": 100,
+		"Dialogue": 100,
+		"Music": 100
+		},
+	"Controls":{
+		"a" : 1
+		},
+	"Graphics":{
+		"WindowMode" : "Fullscreen",
+		"DisplayMonitor" : 0
+		},
+	"General":{
+		"a" : 1,
+		"b" : 3
+		}
+	}
