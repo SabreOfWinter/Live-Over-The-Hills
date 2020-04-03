7d1536d01cd952fe8defed382a7fca08eaf7289f
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-03 21:42:53 +0100

    Menu - Load game animation init

--- menus/main/main.gd
+++ menus/main/main.gd
@@ -4,7 +4,7 @@ func _on_new_game_button_pressed():
 	$animation_player.play("new_game")
 
 func _on_load_game_button_pressed():
-	pass
+	$animation_player.play("load_game")
 
 func _on_settings_button_pressed():
 	pass
