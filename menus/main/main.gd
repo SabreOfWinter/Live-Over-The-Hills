6d62a21b8546355313838afae5b24236b06ffb58
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-04 13:32:47 +0100

    Load game - Create list function added

--- menus/main/main.gd
+++ menus/main/main.gd
@@ -39,14 +39,14 @@ func check_for_files():
 			else:
 				i = i+1
 				#Function for adding nodes with file_name as arg
-				_create_save_game_list(i,file_name)
+				create_save_game_list(i,file_name)
 			file_name = dir.get_next() 
 	else:
 		print("An error occurred when trying to access the path.")
 
 #Using the ListSorter class, this function will sort the arrays in the SaveGameList array with the newest dates going first and descending from there
 func sort_file_list():
-	SaveGameList.sort_custom(ListSorter, "sortList")
+	save_game_list.sort_custom(ListSorter, "sortList")
 
 #This class will compare the date of two files using the two arrays
 class ListSorter:
@@ -60,5 +60,28 @@ class ListSorter:
 		var CompareSecond = (a[1]==b[1] and a[2]==b[2] and a[3]==b[3] and a[4]==b[4] and a[5]==b[5] and a[6] > b[6])
 		
 		return CompareYear or CompareMonth or CompareDay or CompareHour or CompareMinute or CompareSecond
+
+#Creates an array with the current filename and date of last save then adds that array into the SaveGameList array
+func create_save_game_list(i,FileName):
+	#Opens and records the dictionary for the found file.
+	var file = File.new()
+	var FilePath = str("user://save_games/", FileName)
+	var file_dict = {}
+	
+	file.open(FilePath, file.READ)
+	
+	file_dict = parse_json(file.get_line())
+	
+	#Adds the current file's name and last saved date to an array that is then added to the savegame list array
+	var year = file_dict["DateOfLastSave"]["year"]
+	var month = file_dict["DateOfLastSave"]["month"]
+	var day = file_dict["DateOfLastSave"]["day"]
+	var hour = file_dict["DateOfLastSave"]["hour"]
+	var minute = file_dict["DateOfLastSave"]["minute"]
+	var second = file_dict["DateOfLastSave"]["second"]
+	var current_file_name_and_date = []
+	
+	current_file_name_and_date = [FileName, year, month, day, hour, minute, second] #Player Sprite and gold
+	save_game_list.append(current_file_name_and_date)
 
 #SETTINGS
