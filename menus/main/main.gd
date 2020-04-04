13a93bec6a3eb6741f0ecaa44c010f2946da42a3
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-04 13:30:54 +0100

    Load game - List sorter class added

--- menus/main/main.gd
+++ menus/main/main.gd
@@ -48,6 +48,17 @@ func check_for_files():
 func sort_file_list():
 	SaveGameList.sort_custom(ListSorter, "sortList")
 
-
+#This class will compare the date of two files using the two arrays
+class ListSorter:
+	static func sortList(a,b):
+		#year = [1], month  = [2], day  = [3], hour  = [4], minute = [5], second = [6]
+		var CompareYear = a[1] > b[1]
+		var CompareMonth = (a[1]==b[1] and a[2] > b[2])
+		var CompareDay = (a[1]==b[1] and a[2]==b[2] and a[3] > b[3])
+		var CompareHour = (a[1]==b[1] and a[2]==b[2] and a[3]==b[3] and a[4] > b[4])
+		var CompareMinute = (a[1]==b[1] and a[2]==b[2] and a[3]==b[3] and a[4]==b[4] and a[5] > b[5])
+		var CompareSecond = (a[1]==b[1] and a[2]==b[2] and a[3]==b[3] and a[4]==b[4] and a[5]==b[5] and a[6] > b[6])
+		
+		return CompareYear or CompareMonth or CompareDay or CompareHour or CompareMinute or CompareSecond
 
 #SETTINGS
