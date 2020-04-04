f0f71fbd7ba50f523cbab92208723225a67b5745
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-04 13:28:44 +0100

    Load game - Check for files function

--- menus/main/main.gd
+++ menus/main/main.gd
@@ -22,6 +22,25 @@ var save_file_to_delete
 func load_player_save_cards():
 	save_game_list = []
 	
+	check_for_files()
 
+#Checks the user path to find all the save files stored in the folder, it will then create an array for that file's name and date of last save to then be stored in the array SaveGameList
+func check_for_files():
+	var path = "user://save_games/"
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		var i = 0
+		while (file_name != ""):
+			if dir.current_is_dir():
+				pass
+			else:
+				i = i+1
+				#Function for adding nodes with file_name as arg
+				_create_save_game_list(i,file_name)
+			file_name = dir.get_next() 
+	else:
+		print("An error occurred when trying to access the path.")
 
 #SETTINGS
